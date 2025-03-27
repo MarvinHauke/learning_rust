4fb5ef5292544f3879f7f855ca6f404d545382db
PfoRsten <marvin-hauke@hotmail.de>
2025-03-27 13:44:24 +0100

    added Challenge6 for reading and writing to files

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,29 @@
-use std::env;
+use std::io::prelude::*;
+use std::{env, fs};
 
 fn main() {
+    //radding command line arguments:
     if env::args().len() <= 2 {
         println!("The Program needs at least 2 arguments");
         return;
     }
+
+    //reading from files:
+    let filepath = env::args().nth(2).unwrap();
+    let file_content = fs::read_to_string(&filepath).unwrap();
+
+    for line in file_content.lines() {
+        println!("{}", line);
+    }
+
+    //if you want to read bytes for example for bin files or image use read()
+    let file_content = fs::read(&filepath).unwrap(); //this is "shadowind" file_content 
+    println!("the file conntent as vector is {:?}", file_content);
+
+    //this is how you append to a file
+    let mut file = fs::OpenOptions::new().append(true).open(&filepath).unwrap();
+    file.write_all(b"\nappended text")
+        .expect("Failed to write to file");
 
     for (index, argument) in env::args().enumerate() {
         println!("argument {} is {}", index, argument);
