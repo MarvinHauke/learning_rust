89dd57e258255b83b6cf58d0d0ca34b8ac69ef7a
PfoRsten <marvin-hauke@hotmail.de>
2025-04-03 13:14:08 +0200

    Lifetimes

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,9 @@
 //This is about lifetimes in rust.
+//Lifetime Elision Rules:
+// 1. Each input prameter that is a reference is assigned its own lifetime
+//      fn foo <'a>(x:&'a str, y:i32) -> & str{}
+// 2. If there is exactly one input lifetime, assign it to all output lifetimes
+//      fn foo <'a>(x:&'a str) -> &'a str{}
+// 3. if there is a &self or &mut self input parameter, its lifetime will be assigned to all output
+//    lifetimes.
 fn main() {}
